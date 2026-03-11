277ef652603a135f0f99dfdde39b9f7f7ff1008a
hassanasif921 <51042408+hassanasif921@users.noreply.github.com>
2026-03-11 07:19:47 +0500

    Create new.js

---
+++ new.js
@@ -0,0 +1,18 @@
+// function to calculate sum
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+// function that calls the sum function
+function callSum() {
+    let result = sumNumbers(5, 3);
+    console.log("Sum is:", result);
+}
+
+// main function
+function main() {
+    callSum();
+}
+
+// run program
+main();
